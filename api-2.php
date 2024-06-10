13eae7be7e85a87061a08ca4a940e3aed98dd189
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 12:02:35 +0800

    Update api-2.php

--- api-2.php
+++ api-2.php
@@ -1,5 +1,5 @@
 <?php
-$arr=file('bizhi.txt');      /* 此处bizhi.txt中img更改为自己对应的txt文件名*/
+$arr=file('img.txt');      /* 此处bizhi.txt中img更改为自己对应的txt文件名*/
 $n=count($arr)-1;
 for ($i=1;$i<=1;$i++){
 $x=rand(0,$n);
