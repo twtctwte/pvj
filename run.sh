4ce37aff55f4b57c72b55e94cf331fccaeccfc42
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 13:40:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 #需提前安装配置git
 #git config --local user.email "linuxmf1211@qq.com" # 替换为您的 GitHub 邮箱地址
 #git config --local user.name "twtctwte" # 替换为您的 GitHub 用户名
+#ssh-keygen -t rsa -C "这里换上你的邮箱" #配置sshkey
 mkdir -p "wallpapers"
 cd wallpapers
 wget -q -O "$(date +'%Y%m%d%h%I%s').jpg" "https://pix.tawertwtc.xyz/api/random?format=image"
