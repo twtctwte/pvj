ca02f706b309125a26b76373b6cce24a00d3dab9
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 13:23:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,5 @@ cd wallpapers
 wget -q -O "$(date +'%Y%m%d%h%I%s').jpg" "https://pix.tawertwtc.xyz/api/random?format=image"
 cd
 git add wallpapers/*
-else
 git commit -m "添加Pixiv壁纸 $(date +'%Y/%m/%d')"
 git push origin main
-fi
