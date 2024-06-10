9c457d7755e399d3d5e9ea0e3707ba4b81cb4bc5
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 13:47:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,5 @@
 #git config --local user.email "linuxmf1211@qq.com" # 替换为您的 GitHub 邮箱地址
 #git config --local user.name "twtctwte" # 替换为您的 GitHub 用户名
 #ssh-keygen -t rsa -C "这里换上你的邮箱" #配置sshkey
-mkdir -p "wallpapers"
-cd wallpapers
-wget -q -O "$(date +'%Y%m%d%h%I%s').jpg" "https://pix.tawertwtc.xyz/api/random?format=image"
-cd
-git add wallpapers/*
-git commit -m "添加Pixiv壁纸 $(date +'%Y/%m/%d')"
-git push origin main
-cd
-#rm  -r  wallpapers #上传图片至github后是否删除服务器图片
+mkdir -p "wallpapers"&&cd wallpapers&&wget -q -O "$(date +'%Y%m%d%h%I%s').jpg" "https://pix.tawertwtc.xyz/api/random?format=image"&&cd&&git add wallpapers/*&&git commit -m "添加Pixiv壁纸 $(date +'%Y/%m/%d')"&&git push origin main&&cd&&rm  -r  wallpapers
 
