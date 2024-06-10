631a939ac758e058f7656c80475333d0026e6af6
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 13:00:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #需提前安装配置git
 mkdir -p "wallpapers"
 cd wallpapers
-wget -O https://pix.tawertwtc.xyz/api/random?format=image
+wget -q -O "$FILENAME" "https://pix.tawertwtc.xyz/api/random?format=image"
 cd
