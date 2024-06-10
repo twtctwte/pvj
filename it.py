f13e4377f9f52f7ccc827c8565cd494117315e96
twtctwte <132817665+twtctwte@users.noreply.github.com>
2024-06-10 12:14:37 +0800

    Update it.py

--- it.py
+++ it.py
@@ -1,7 +1,7 @@
 import os
 paths = r'./wallpapers'  # 储存图片的文件夹路径
 f = open('img.txt', 'w')
-filenames = os.listdir(paths)  # 读取图片名称
+filenames = os.listdir(paths)  # 读取图片
 for filename in filenames:
     out = filename
     f.write("https://www.jsdelivr.ren/gh/twtctwte/pixiv@main/wallpapers/"+out+'\n') #cdn
